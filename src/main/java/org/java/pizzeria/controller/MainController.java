d84ab1fa03fc740771b85dd61634145bd30609c9
mcspe <mcspezzaferro@gmail.com>
2023-10-09 19:52:34 +0200

    [add] create

--- src/main/java/org/java/pizzeria/controller/MainController.java
+++ src/main/java/org/java/pizzeria/controller/MainController.java
@@ -3,14 +3,20 @@ package org.java.pizzeria.controller;
 import java.util.List;
 
 import org.java.pizzeria.db.pojo.Pizza;
+import org.java.pizzeria.db.pojo.PizzaDTO;
 import org.java.pizzeria.db.serv.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/")
@@ -26,8 +32,7 @@ public class MainController {
 												: pizzaService.search(pizza);
 		model.addAttribute("pizzas", pizzas);
 		
-		return "index";
-		
+		return "index";	
 	}
 	
 	@GetMapping("/details/{id}")
@@ -46,4 +51,36 @@ public class MainController {
 		return "detail";
 	}
 
+	@GetMapping("/create")
+	public String getCreate(Model model) {
+		
+		String pageTitle = "Add a new Pizza";
+		
+		model.addAttribute("pageTitle", pageTitle);
+		model.addAttribute("pizzaDTO", new PizzaDTO());
+		
+		return "create";
+	}
+	
+	@PostMapping("/create")
+	public String store(
+			@Valid @ModelAttribute PizzaDTO pizzaDTO, 
+			BindingResult bindingResult, 
+			Model model
+			) {
+		
+		if(bindingResult.hasErrors()) {
+			System.out.println("Error:");
+			bindingResult.getAllErrors().forEach(System.out::println);
+					
+			return "create";
+		}
+		
+		Pizza pizza = new Pizza(pizzaDTO.getName(),	pizzaDTO.getDescription(), pizzaDTO.getImg(), pizzaDTO.getIntPrice());
+	
+		pizzaService.save(pizza);
+		
+		return "redirect:/details/" + pizza.getId();
+	}
+	
 }
